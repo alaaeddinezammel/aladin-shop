fc94aaf58c9c3f38df6f55b0b1c7acfa381c4593
Jenkinsgit <alaaeddine.zammel@esprit.tn>
2018-11-20 14:33:20 +0100

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBsrB4WEEOKMhV85a0UbNnPEsVEouk_unY',
+    authDomain: 'aladin-shop-5b628.firebaseapp.com',
+    databaseURL: 'https://aladin-shop-5b628.firebaseio.com',
+    projectId: 'aladin-shop-5b628',
+    storageBucket: 'aladin-shop-5b628.appspot.com',
+    messagingSenderId: '488682974016'
+  }
+};
